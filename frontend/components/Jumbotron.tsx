141dda841b02701b4f4836679b8548e4ccfee069
thanapongsj <thanapongsj1996@gmail.com>
2022-03-29 23:50:17 +0700

    (frontend)feat: add jumbotron component

---
+++ frontend/components/Jumbotron.tsx
@@ -0,0 +1,11 @@
+import styles from '../styles/Jumbotron.module.css'
+
+const Jumbotron = () => {
+    return (
+        <div className={styles.jumbotron}>
+            <h1>Todo App</h1>
+        </div>
+    )
+}
+
+export default Jumbotron
