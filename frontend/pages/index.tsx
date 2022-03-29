7be983721b4eec854b222e59d07accc4eddef187
thanapongsj <thanapongsj1996@gmail.com>
2022-03-29 22:26:06 +0700

    (frontend)feat: add bootstrap css

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -17,6 +17,8 @@ const Home: NextPage = () => {
           Welcome to <a href="https://nextjs.org">Next.js!</a>
         </h1>
 
+        <div className='btn btn-warning'>TEST</div>
+
         <p className={styles.description}>
           Get started by editing{' '}
           <code className={styles.code}>pages/index.tsx</code>
