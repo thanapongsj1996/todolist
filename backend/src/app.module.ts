e822bd276baaaf03fc475b87b974e586aba23d77
thanapongsj <thanapongsj1996@gmail.com>
2022-03-31 02:33:17 +0700

    (backend)refactor: add env, use docker-compose to run backend service

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,14 +1,17 @@
 /* istanbul ignore file */
+import * as dotenv from 'dotenv'
 import { Module } from '@nestjs/common'
 import { TypeOrmModule } from '@nestjs/typeorm'
 import { SubtaskModule } from './modules/subtask/subtask.module'
 import { TodoModule } from './modules/todo/todo.module'
 
+dotenv.config()
+
 @Module({
   imports: [
     TypeOrmModule.forRoot({
       type: 'postgres',
-      url: 'postgres://localhost:5432/todos',
+      url: process.env.DATABASE_URL,
       autoLoadEntities: true,
       synchronize: true
     }),
