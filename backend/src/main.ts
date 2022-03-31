91a783b270bb350ab23f2b84fa04c9b7c15cc2a2
thanapongsj <thanapongsj1996@gmail.com>
2022-04-01 05:08:37 +0700

    (backend)refactor: add cors

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -5,6 +5,7 @@ import { AppModule } from './app.module'
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
   app.setGlobalPrefix('/api/v1/')
+  app.enableCors()
   await app.listen(8000)
 }
 bootstrap()
